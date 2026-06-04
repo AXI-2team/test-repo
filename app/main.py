6cb4c02e07ae20d4a84d39c02012944b23c10838
sosh0726-jpg <sosh0726@gmail.com>
2026-06-04 17:18:24 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -23,9 +23,6 @@ st.set_page_config(
     layout="wide"
 )
 
-vs-psh
-st.title("🤖 [VS-GitHub] RAG Demo (MariaDB VectorStore 기반)")
-=======
 
 st.title("🤖[GITHUB] RAG Demo <VSCODE GIT 연동 test중...>")
 master
